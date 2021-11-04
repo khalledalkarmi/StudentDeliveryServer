68fe70409af8027266a2029ff9f7960b10aa8001
khalled alkarmi <khalled_95@hotmail.com>
2021-11-05 00:34:24 +0200

    init

---
+++ src/main/java/com/wise/studentdelivery/StudentDeliveryServerApplication.java
@@ -0,0 +1,13 @@
+package com.wise.studentdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDeliveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDeliveryServerApplication.class, args);
+	}
+
+}
