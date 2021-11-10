4a3b524eb00531433b349a206e8855e634701e25
khalled alkarmi <khalled_95@hotmail.com>
2021-11-10 11:19:56 +0200

    Add mongoDB docker-compose.yaml

--- src/main/java/com/wise/studentdelivery/StudentDeliveryServerApplication.java
+++ src/main/java/com/wise/studentdelivery/StudentDeliveryServerApplication.java
@@ -8,6 +8,10 @@ public class StudentDeliveryServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentDeliveryServerApplication.class, args);
+
 	}
 
+	public void print(){
+		
+	}
 }
