455284650aff5cdd57f9112a16277ad5be630f3c
khalled alkarmi <khalled_95@hotmail.com>
2021-11-12 01:00:59 +0200

    add GET request in UserController

---
+++ src/main/java/com/wise/studentdelivery/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.wise.studentdelivery.controller;
+
+import com.wise.studentdelivery.model.User;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@AllArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> fetchAllStudent(){
+        return userService.getAllUsers();
+    }
+}
