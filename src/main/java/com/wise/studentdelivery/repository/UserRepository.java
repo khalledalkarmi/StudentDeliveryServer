6f8e6edfff24a452b34fe133b04cce241538cdf8
khalled alkarmi <khalled_95@hotmail.com>
2021-11-12 00:18:34 +0200

    add custom method to find user by email, phone number and student number.

--- src/main/java/com/wise/studentdelivery/repository/UserRepository.java
+++ src/main/java/com/wise/studentdelivery/repository/UserRepository.java
@@ -3,6 +3,11 @@ package com.wise.studentdelivery.repository;
 import com.wise.studentdelivery.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository <User,String>{
+import java.util.Optional;
 
+public interface UserRepository extends MongoRepository <User,String >{
+
+    Optional<User> findUserByEmile(String email);
+    Optional<User> findUserByPhoneNumber(int phoneNumber);
+    Optional<User> findUserByStudentNumber(Long studentNumber);
 }
