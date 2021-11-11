3fcc10ea2b92d806f8bdd587e3959c8dd8ccefea
khalled alkarmi <khalled_95@hotmail.com>
2021-11-11 23:39:53 +0200

    add lombok AllArgsConstructor

--- src/main/java/com/wise/studentdelivery/model/Car.java
+++ src/main/java/com/wise/studentdelivery/model/Car.java
@@ -1,8 +1,10 @@
 package com.wise.studentdelivery.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Car {
     private String carModel;
     private String carColor;
