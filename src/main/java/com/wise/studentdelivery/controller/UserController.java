0e90efe29d7679a1ef1a0e2645862d04cdb5c75e
khalled alkarmi <khalled_95@hotmail.com>
2021-11-12 02:18:53 +0200

    Add Logger

--- src/main/java/com/wise/studentdelivery/controller/UserController.java
+++ src/main/java/com/wise/studentdelivery/controller/UserController.java
@@ -2,6 +2,8 @@ package com.wise.studentdelivery.controller;
 
 import com.wise.studentdelivery.model.User;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,8 +24,11 @@ public class UserController {
 
     private final UserService userService;
 
+    private final Logger LOG = LoggerFactory.getLogger(getClass());
+
     @GetMapping
     public List<User> fetchAllStudent(){
+        LOG.info("Getting all Users.");
         return userService.getAllUsers();
     }
 }
