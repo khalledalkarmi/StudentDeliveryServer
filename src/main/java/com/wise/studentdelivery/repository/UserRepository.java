63709d4a3e30d4161070b099b05c69a4116f1c2b
khalled alkarmi <khalled_95@hotmail.com>
2021-11-13 18:31:20 +0200

    Add findUsersByUniName

--- src/main/java/com/wise/studentdelivery/repository/UserRepository.java
+++ src/main/java/com/wise/studentdelivery/repository/UserRepository.java
@@ -10,4 +10,5 @@ public interface UserRepository extends MongoRepository <User,String >{
     Optional<User> findUserByEmile(String email);
     Optional<User> findUserByPhoneNumber(int phoneNumber);
     Optional<User> findUserByStudentNumber(Long studentNumber);
+    Optional<User> findUsersByUniName(String uniName);
 }
