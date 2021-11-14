f20d21f27f7ecbdfa2522a85f4f0a8a37c73e72e
khalled alkarmi <khalled_95@hotmail.com>
2021-11-14 09:02:53 +0200

    add fetchByEmail fetchByStudentNumber fetchStudentByUni method

--- src/main/java/com/wise/studentdelivery/repository/UserRepository.java
+++ src/main/java/com/wise/studentdelivery/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.wise.studentdelivery.repository;
 import com.wise.studentdelivery.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
 import java.util.Optional;
 //TODO: add findUserByCarNumber
 public interface UserRepository extends MongoRepository <User,String >{
@@ -10,6 +11,6 @@ public interface UserRepository extends MongoRepository <User,String >{
     Optional<User> findUserByEmile(String email);
     Optional<User> findUserByPhoneNumber(int phoneNumber);
     // to find student by id number
-    Optional<User> findUserByStudentNumber(Long studentNumber);
-    Optional<User> findUsersByUniName(String uniName);
+    Optional<User> findUserByStudentNumber(String studentNumber);
+    Optional<List<User>> findUsersByUniName(String uniName);
 }
