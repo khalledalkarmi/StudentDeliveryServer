7819c49b9e4916b9c4d60f89cb80c93cbff39f88
khalled alkarmi <khalled_95@hotmail.com>
2021-12-04 22:34:01 +0200

    add sendEmail and test it

--- src/main/java/com/wise/studentdelivery/controller/UserService.java
+++ src/main/java/com/wise/studentdelivery/controller/UserService.java
@@ -3,6 +3,9 @@ package com.wise.studentdelivery.controller;
 import com.wise.studentdelivery.model.User;
 import com.wise.studentdelivery.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,7 +14,10 @@ import java.util.Optional;
 @AllArgsConstructor
 @Service
 public class UserService {
+
     private final UserRepository userRepository;
+
+    private JavaMailSender mailSender;
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
@@ -35,6 +41,15 @@ public class UserService {
 
     public void addUser(User user) {
         userRepository.save(user);
+    }
+
+    public void sendMail(String to, String subject, String body)
+    {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
     }
 
 
